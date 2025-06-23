a3c8257956b6a2714dbdd202cc07da37d01ec08c
vijayanandkarthik <vijayanandkarthik2006@gmail.com>
2025-06-23 00:47:05 -0400

    message

---
+++ scope/func_scope.js
@@ -0,0 +1,11 @@
+function show(){
+    var funcVar= "its function variable";
+    let funcConst= "its function constant";
+    const funcLet= "its function let";
+
+console.log(funcVar); 
+console.log(funcLet); 
+console.log(funcConst);    
+}
+show();
+
